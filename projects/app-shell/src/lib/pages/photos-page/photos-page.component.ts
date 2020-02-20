b2efde5129444b0c8aedf267e0658d0db9da7c6c
Kacy Sommers <kacy@dev.adaptiva.com>
2020-02-19 16:56:50 -0800

    starting to set up lazy loading on scroll

--- projects/app-shell/src/lib/pages/photos-page/photos-page.component.ts
+++ projects/app-shell/src/lib/pages/photos-page/photos-page.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnDestroy, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Subscription, Subject } from 'rxjs';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { PhotosService } from 'projects/core/src/lib/services/photos.service';
 import { Filters } from '@kk/core';
-import { take } from 'rxjs/operators';
+import { take, takeUntil } from 'rxjs/operators';
 import { Location } from '@angular/common';
+
+interface PhotosPageParams extends Params {
+  filters: string; // cleaning,painting,landscaping
+}
+
+interface ImageMap {
+  [filters: string]: string[]
+}
+
+interface ImageData {
+  src: string;
+  filter: Filters;
+  loaded: boolean;
+}
 
 @Component({
   selector: 'kk-photos-page',
   templateUrl: './photos-page.component.html',
   styleUrls: ['./photos-page.component.scss']
 })
-export class PhotosPageComponent implements OnDestroy {
+export class PhotosPageComponent implements OnInit, OnDestroy {
   public filters = [
     Filters.CLEANING,
     Filters.COMMERCIAL,
@@ -20,123 +34,152 @@ export class PhotosPageComponent implements OnDestroy {
     Filters.RESIDENTIAL
   ];
 
-  public selectedFilters: Filters[];
-  private _params$: Subscription;
+  private _destroy = new Subject();
+
+  private _initialVisit = true;
+
   private _imageCache = new Map<Filters, string[]>();
-  public displayedImages = new Map<Filters, string[]>();
+
+  private _currentImages: ImageData[] = [];
+
+  public selectedFilters: Filters[];
+
+  private _currentIndex = 0;
+
+  private _observer: IntersectionObserver;
 
   constructor(
+    private _router: Router,
     private _route: ActivatedRoute,
     private _photosService: PhotosService,
-    private _location: Location,
-    private _router: Router
+    private _renderer: Renderer2
   ) {
-    this._params$ = this._route.queryParams.subscribe(params => {
-      let currentFilters = this.selectedFilters ? this.selectedFilters : this.filters;
-      if (params && params.hasOwnProperty('filters')) {
-        const filters = params.filters
-          .replace(', ', ',')
-          .split(',')
-          .filter((f: Filters) => this.filters.includes(f));
-        if (filters.length) {
-          currentFilters = filters;
+    this._route.queryParams
+      .pipe(takeUntil(this._destroy))
+      .subscribe(
+        (params: PhotosPageParams) => {
+          if (this._initialVisit) {
+            this._initialVisit = false;
+            this.selectedFilters = (params && params.filters) ? params.filters
+              .replace(' ', '')
+              .split(',')
+              .filter((f: Filters) => this.filters.includes(f)) as Filters[]
+              : [...this.filters];
+            this.updateUrl();
+            this.fetchImages(this.selectedFilters);
+          }
         }
-      }
-      if (!this.selectedFilters) {
-        this.selectedFilters = currentFilters;
-        this.updateUrl();
-      }
-      this.getImageUrls(currentFilters);
-    });
+      );
   }
 
-  ngOnDestroy() {
-    this._params$.unsubscribe();
-  }
-
-  public getImageUrls(filters: Filters[]) {
-    const notCached = filters.filter(f => {
-      if (this._imageCache.get(f)) {
-        if (!this.displayedImages.get(f)) {
-          this.displayedImages.set(f, this._imageCache.get(f));
+  ngOnInit(): void {
+    this._observer = new IntersectionObserver(
+      () => {
+        this._currentIndex += 4;
+        if (this._currentIndex >= this._currentImages.length) {
+          this._observer.disconnect();
         }
-        return false;
+        this.appendImages();
+      },
+      {
+        rootMargin: '0px',
+        threshold: 0.5
       }
-      return true;
-    });
-    console.log('NOT CACHED:::: ', notCached)
-    if (notCached.length) {
-      this._photosService.getImages(notCached)
-        .pipe(take(1))
-        .subscribe(
-          result => {
-            if (result && result.resources) {
-              console.log('RESULT:::: ', result.resources)
-              const imgsSorted = {};
-              result.resources.forEach(img => {
-                const filter = this.parseFolder(img.folder);
-                if (imgsSorted[filter]) {
-                  imgsSorted[filter].push(img.url);
-                } else {
-                  imgsSorted[filter] = [img.url];
-                }
-              });
-              for (const filter in imgsSorted) {
-                if (this.filters.includes(filter as Filters) && imgsSorted[filter]) {
-                  this.displayedImages.set(filter as Filters, imgsSorted[filter]);
-                  this._imageCache.set(filter as Filters, imgsSorted[filter]);
-                }
-              }
-            }
-          },
-          err => console.error(err)
-        );
-    }
+    );
+    this._observer.observe(document.querySelector('footer'));
   }
 
-  public toggleFilter(filter: Filters) {
-    if (!this.selectedFilters.includes(filter)) {
-      this.selectFilter(filter);
+  ngOnDestroy(): void {
+    this._destroy.next(false);
+    this._destroy.complete();
+    this._observer.disconnect();
+  }
+
+  public toggleFilter(filter: Filters): void {
+    if (this.selectedFilters.includes(filter)) {
+      // remove filter
+      this.selectedFilters.splice(this.selectedFilters.indexOf(filter), 1);
     } else {
-      this.deselectFilter(filter);
+      // add filter
+      this.selectedFilters.push(filter);
+      // fetch more images if this filter hasn't been used yet
+      if (!this._imageCache.has(filter)) {
+        this.fetchImages([filter])
+      } else {
+        this.appendImages();
+      }
     }
     this.updateUrl();
   }
 
-  private loadImages(folder: string) {
-    /* webpackInclude: /\.json$/ */
-    /* webpackExclude: /\.noimport\.json$/ */
-    /* webpackChunkName: "my-chunk-name" */
-    /* webpackMode: "lazy" */
-    /* webpackPrefetch: true */
-    /* webpackPreload: true */
+  private fetchImages(filters: Filters[]): void {
+    this._photosService.getImages(filters)
+      .pipe(take(1))
+      .subscribe(
+        (imgMap: ImageMap) => {
+          for (const filter in imgMap) {
+            // set each new filter in the image cache
+            if (filter && imgMap[filter]) {
+              this._imageCache.set(filter as Filters, imgMap[filter]);
+              imgMap[filter].forEach(url => {
+                this._currentImages.push({
+                  src: url,
+                  filter: filter as Filters,
+                  loaded: false
+                });
+              });
+              this.appendImages();
+            }
+          }
+        },
+        err => console.error(err)
+      );
   }
 
-  private selectFilter(filter: Filters): void {
-    this.selectedFilters = [...this.selectedFilters, filter];
+  private hideImages(filter: Filters): void {
+    const toHide = document.querySelectorAll(`.kk-photo-${filter}`);
+    toHide.forEach(el => el.classList.add('kk-photo-hidden'));
+    this._currentIndex += toHide.length;
   }
 
-  private deselectFilter(filter: Filters): void {
-    this.selectedFilters = this.selectedFilters.filter(f => f !== filter);
-    this.displayedImages.delete(filter);
+  private showImages(filter: Filters): void {
+    const toShow = document.querySelectorAll(`.kk-photo-${filter}`);
+    toShow.forEach(el => el.classList.remove('kk-photo-hidden'));
+    this._currentIndex -= toShow.length;
   }
 
-  private updateUrl() {
-    console.log('Update')
+  private appendImages(): void {
+    console.log('IMage data', this._currentIndex, this._currentImages)
+    for (let i = 0; i < this._currentIndex; i++) {
+      const imageData = this._currentImages[i];
+      if (imageData && !imageData.loaded) {
+        imageData.loaded = true;
+        const imgEl: HTMLImageElement = document.createElement('img');
+        imgEl.src = imageData.src;
+        imgEl.alt = `${imageData.filter} photo`;
+        imgEl.style.maxWidth = '100%';
+        imgEl.style.width = 'auto';
+        imgEl.style.maxHeight = '100%';
+        const imgWrap: HTMLDivElement = document.createElement('div');
+        imgWrap.classList.add(`kk-photo-${imageData.filter}`);
+        imgWrap.classList.add('kk-photo-wrap');
+        imgWrap.style.height = '100%';
+        imgWrap.style.background = '#fff';
+        imgWrap.style.display = 'flex';
+        imgWrap.style.alignItems = 'center';
+        imgWrap.style.justifyContent = 'center';
+        imgWrap.appendChild(imgEl);
+        document.querySelector('.photos-section-wrap').appendChild(imgWrap);
+      }
+    }
+  }
+
+  private updateUrl(): void {
     this._router.navigate([], {
-      relativeTo: this._route,
-      queryParams: { filters: this.selectedFilters.join(',') },
+      queryParams: {
+        filters: this.selectedFilters.join(',')
+      },
       queryParamsHandling: 'merge'
     });
-  }
-
-  private parseFolder(folder: string): string {
-    // 2K Homecare/painting
-    const segments = folder.split('/');
-    return segments[segments.length - 1];
-  }
-
-  public trackByFn(index: number): number {
-    return index;
   }
 }
